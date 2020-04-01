078715a6d252a54028220aa3d01706da722ae44e
Munioro <munioro27@gmail.com>
2020-04-01 12:22:34 -0500

    run inquirer and create new employee

--- Develop/app.js
+++ Develop/app.js
@@ -4,32 +4,82 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
+
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+
 const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+
+const inputInfo = [
+    {
+        type: 'input',
+        name: 'Name',
+        message:'Enter First and Last name:',
+    },
+
+    {
+        type: 'number',
+        name: 'ID',
+        message: 'Enter employee\'s ID:',
+    },
+
+    {
+        type: 'input',
+        name: 'Email',
+        message: 'Enter employee\'s Email:',
+    },
+];
+
+const inputMan = [
+    ...inputInfo,
+    {
+        type: 'number',
+        name: 'OfficeNumber',
+        message: 'Enter Office Number:',
+    },
+];
+
+const inputEng = [
+    ...inputInfo,
+    {
+        type: 'input',
+        name: 'GitHub',
+        message: 'Enter GitHub username:',
+    },
+];
+
+const inputIntrn = [
+    ...inputInfo,
+    {
+        type: 'input',
+        name: 'School',
+        message: 'Enter Intern\'s school:',
+    },
+];
+function inqFunc(){
+inquirer.prompt({
+    type: 'list',
+    name: 'Role',
+    message: 'Select employee\'s role',
+    choices: ['Manager', 'Engineer', 'Intern']
+}).then((answer) => {
+    if(answer.Role === 'Manager'){
+        inquirer.prompt(inputMan).then((ans) => {
+            console.log('manager was chosen');
+            const newManager = new Manager(ans.Name, ans.ID, ans.Email, ans.OfficeNumber)
+            console.log(newManager);
+        })
+    }else if(answer.Role === 'Engineer'){
+        inquirer.prompt(inputEng).then((ans) => {
+            const newEngineer = new Engineer(ans.Name, ans.ID, ans.Email, ans.GitHub);
+            console.log(newEngineer);
+        })
+    }else if(answer.Role === 'Intern'){
+        inquirer.prompt(inputIntrn).then((ans) => {
+            const newIntern = new Intern(ans.Name, ans.ID, ans.Email, ans.School)
+            console.log(newIntern)
+        })
+    }
+})
+};
+inqFunc()
