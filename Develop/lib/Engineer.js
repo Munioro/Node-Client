34b55bc2aa627c9b65b8f1b8ccbb6f024b03bfac
Munioro <munioro27@gmail.com>
2020-04-01 06:43:53 -0500

    passed all tests

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -6,11 +6,14 @@ class Engineer extends Employee{
         super(name, id, email)
         this.github = github;
     };
-    getGithub(){};
+    getGithub(){
+        return this.github;
+    };
     getRole(){
-        return Engineer;
+        return 'Engineer';
     };
 }
+module.exports = Engineer;
 /*github // GitHub username
 
 getGithub()
